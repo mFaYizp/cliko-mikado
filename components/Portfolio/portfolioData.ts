7dedf3b0da3310173be6cb6b84349c78942b0f02
sreya-mikado <sreya@mikado.biz>
2025-06-12 19:31:31 +0530

    architecture photography included

--- components/Portfolio/portfolioData.ts
+++ components/Portfolio/portfolioData.ts
@@ -5,34 +5,51 @@ const PortfolioData = [
     industry: "Beverages",
     service: "Product Photography",
     heroBg:
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/New.jpg",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/product-banner.webp",
     image:
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/1).jpg",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/highlight.webp",
     about: {
       title: "Highlight",
       description:
         "Our product photography service focuses on creating high-quality, conversion-ready visuals designed for both digital and print marketing platforms. Whether it's for an e-commerce catalog, Amazon listing, or social media campaign, we ensure every image captures the product's unique features, textures, and intended appeal.",
-      src: "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/1).jpg",
+      src: "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/highlight.webp",
     },
     images: [
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/1.jpg",
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/2.jpg",
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/1-img.jpg",
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/2.jpg",
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/3.jpg",
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-6.jpg",
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-7.jpg",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img1.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img2.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-3.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-4.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-5.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-6.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/img-7.webp",
 
     ],
   },
   {
-    slug: "2",
-    title: "",
-    type: "",
-    category: "product",
+    slug: "architecture",
+    title: "Architecture Photography",
+    industry: "Beverages",
+    service: "Product Photography",
+    heroBg:
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-hero.webp",
     image:
-      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/1.jpg",
-    href: "/portfolio/2",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-banner.webp",
+    about: {
+      title: "Highlight",
+      description:
+        "Our product photography service focuses on creating high-quality, conversion-ready visuals designed for both digital and print marketing platforms. Whether it's for an e-commerce catalog, Amazon listing, or social media campaign, we ensure every image captures the product's unique features, textures, and intended appeal.",
+      src: "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-banner.webp",
+    },
+    images: [
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-1.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-2.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-3.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-4.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/arch-5.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/empty-room-with-chairs-desks.webp",
+      "https://mikado-products.blr1.cdn.digitaloceanspaces.com/cliko/portfolio/room-interior-design.webp",
+
+    ],
   },
   {
     slug: "3",
