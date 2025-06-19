992042e5c1d1efe41fa50a950d5825023e4e91c4
mFaYizp <fayizfaz78@gmail.com>
2025-06-19 20:08:45 +0530

    inner new layout

--- components/Portfolio/PortfolioTwo.tsx
+++ components/Portfolio/PortfolioTwo.tsx
@@ -15,101 +15,112 @@ const PortfolioTwo = ({ images }: { images: string[] }) => {
     <section className="w-full h-full py-20">
       <div className="w-auto mx-5 md:mx-10 lg:mx-20">
         <div className="hidden md:flex flex-col gap-20">
-          {Array.from({ length: Math.ceil(images.length / 10) }).map(
-            (_, groupIndex) => (
-              <div key={groupIndex} className="flex flex-col gap-20">
-                {/* Second row - Two items */}
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-                  {[0, 1].map((offset) => {
-                    const baseIndex = groupIndex * 10;
-                    const item = images[baseIndex + offset];
-                    if (!item) return null;
+          {Array.from({ length: Math.ceil(images.length / 8) }).map(
+            (_, groupIndex) => {
+              const baseIndex = groupIndex * 8;
+              return (
+                <div key={groupIndex} className="flex flex-col gap-20">
+                  {/* First row - Two items */}
+                  {images[baseIndex] || images[baseIndex + 1] ? (
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                      {[0, 1].map((offset) => {
+                        const item = images[baseIndex + offset];
+                        if (!item) return null;
 
-                    return (
-                      <div
-                        key={item}
-                        className="w-full h-full relative group overflow-hidden rounded-lg"
-                      >
-                        <Image
-                          src={item}
-                          alt={"image"}
-                          width={800}
-                          height={800}
-                          className="w-full h-auto object-cover"
-                        />
-                      </div>
-                    );
-                  })}
+                        return (
+                          <div
+                            key={item}
+                            className="w-full h-full relative group overflow-hidden rounded-lg"
+                          >
+                            <Image
+                              src={item}
+                              alt={"image"}
+                              width={800}
+                              height={800}
+                              className="w-full h-auto object-cover"
+                            />
+                          </div>
+                        );
+                      })}
+                    </div>
+                  ) : null}
+                  {/* Second row - Three items */}
+                  {images[baseIndex + 2] ||
+                  images[baseIndex + 3] ||
+                  images[baseIndex + 4] ? (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+                      {[2, 3, 4].map((offset) => {
+                        const item = images[baseIndex + offset];
+                        if (!item) return null;
+
+                        return (
+                          <div
+                            key={item}
+                            className="w-full h-full relative group overflow-hidden rounded-lg"
+                          >
+                            <Image
+                              src={item}
+                              alt={"image"}
+                              width={600}
+                              height={600}
+                              className="w-full h-auto object-cover"
+                            />
+                          </div>
+                        );
+                      })}
+                    </div>
+                  ) : null}
+
+                  {/* Third row - Two items */}
+                  {images[baseIndex + 5] || images[baseIndex + 6] ? (
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                      {[5, 6].map((offset) => {
+                        const item = images[baseIndex + offset];
+                        if (!item) return null;
+
+                        return (
+                          <div
+                            key={item}
+                            className="w-full h-full relative overflow-hidden rounded-lg"
+                          >
+                            <Image
+                              src={item}
+                              alt={"image"}
+                              width={600}
+                              height={600}
+                              className="w-full h-auto object-cover"
+                            />
+                          </div>
+                        );
+                      })}
+                    </div>
+                  ) : null}
+                  {images[baseIndex + 7] ? (
+                    <div className="grid grid-cols-1 gap-10">
+                      {[7].map((offset) => {
+                        const item = images[baseIndex + offset];
+                        if (!item) return null;
+
+                        return (
+                          <div
+                            key={item}
+                            className="w-full h-full relative overflow-hidden rounded-lg"
+                          >
+                            <Image
+                              src={item}
+                              alt={"image"}
+                              width={1200}
+                              height={1200}
+                              className="w-full h-auto object-cover"
+                            />
+                          </div>
+                        );
+                      })}
+                    </div>
+                  ) : null}
                 </div>
-
-                {/* Third row - Three items */}
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-                  {[2, 3, 4].map((offset) => {
-                    const item = images[groupIndex * 10 + offset];
-                    if (!item) return null;
-
-                    return (
-                      <div
-                        key={item}
-                        className="w-full h-full relative group overflow-hidden rounded-lg"
-                      >
-                        <Image
-                          src={item}
-                          alt={"image"}
-                          width={600}
-                          height={600}
-                          className="w-full h-auto object-cover"
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-
-                {/* Fourth row - Two items */}
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-                  {[5, 6].map((offset) => {
-                    const item = images[groupIndex * 10 + offset];
-                    if (!item) return null;
-
-                    return (
-                      <div
-                        key={item}
-                        className="w-full h-full relative group overflow-hidden rounded-lg"
-                      >
-                        <Image
-                          src={item}
-                          alt={"image"}
-                          width={600}
-                          height={600}
-                          className="w-full h-auto object-cover"
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-                  {[7, 8, 9].map((offset) => {
-                    const item = images[groupIndex * 10 + offset];
-                    if (!item) return null;
-
-                    return (
-                      <div
-                        key={item}
-                        className="w-full h-full relative group overflow-hidden rounded-lg"
-                      >
-                        <Image
-                          src={item}
-                          alt={"image"}
-                          width={600}
-                          height={600}
-                          className="w-full h-auto object-cover"
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            )
+              );
+            }
           )}
         </div>
 
