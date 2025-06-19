527a8ff8990cd2ac02d53e12fbfa7055d489acc7
sreya-mikado <sreya@mikado.biz>
2025-06-19 19:19:47 +0530

    content for about updated

--- components/About/AboutStory.tsx
+++ components/About/AboutStory.tsx
@@ -8,8 +8,8 @@ const AboutStory = () => {
     <GoalSection
       section="The Story"
       desc={[
-        "Hello, we going manor who did. Do ye is considered occasion directly that. May ecstatic did surprise elegance the ignorant age. Own her miss cold. It so numerous if outlived possession.Hello, we going manor who did. Do ye is considered occasion directly that. May ecstatic did surprise elegance the ignorant age. Own her miss cold. It so numerous if outlived possession.",
-        "Fat new smallness few supposing suspicion two. Course sir awesome website templates add entire design. How one dull get busy dare far. At principle perfectly by sweetness do. As mr started arrival.",
+        "At Clicko, we specialize in delivering high-impact corporate photography that captures your brand’s essence with clarity and creativity. From architectural photography that highlights structural elegance to product shoots that drive conversions, corporate portraits that establish professionalism, and fashion photography that defines identity — we bring every visual to life with purpose.",
+        "With a team of skilled photographers, creative directors, and post-production experts, Clicko provides end-to-end photography solutions tailored for brands across industries. Whether you’re a real estate developer, an e-commerce startup, a fashion label, or a corporate enterprise, our goal is to help you stand out through powerful, brand-aligned imagery.",
       ]}
     />
   );
